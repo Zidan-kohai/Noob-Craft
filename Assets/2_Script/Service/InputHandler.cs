309045cd2ddaf50856d2f8154896357b0a27d9d1
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2024-06-14 15:20:38 +0300

    add player controler

---
+++ Assets/2_Script/Service/InputHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public float HorizontalMovement { get; private set; }
+
+    public float VerticalMovement { get; private set; }
+
+    public float HorizontalSwipe { get; private set; }
+
+    public float VerticalSwipe { get; private set; }
+
+
+
+    private void Update()
+    {
+        HorizontalMovement = Input.GetAxis("Horizontal");
+        VerticalMovement = Input.GetAxis("Vertical");
+
+        HorizontalSwipe = Input.GetAxis("Mouse X");
+        VerticalSwipe = Input.GetAxis("Mouse Y");
+    }
+}
